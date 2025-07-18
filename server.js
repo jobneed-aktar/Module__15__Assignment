806829430549f2f0cb139f072503bd3daa744be0
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-07-18 21:23:44 +0600

    Final stage

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// server.js
 const app = require('./app');
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} http://localhost:5000`);
 });
